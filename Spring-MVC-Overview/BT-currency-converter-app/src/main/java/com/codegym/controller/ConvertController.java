ceaef75d6d605c1266d7915ab9e77539ff1e85f3
Hữu Trần Ngọc <ngochuutran2702@gmail.com>
2021-12-22 21:56:56 +0700

    Ứng dụng kiểm tra email hợp lệ sử dụng Maven

--- Spring-MVC-Overview/BT-currency-converter-app/src/main/java/com/codegym/controller/ConvertController.java
+++ Spring-MVC-Overview/BT-currency-converter-app/src/main/java/com/codegym/controller/ConvertController.java
@@ -2,22 +2,19 @@ package com.codegym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class ConvertController  {
 
-    @RequestMapping(value = { "/index"}, method = RequestMethod.GET)
+    @GetMapping("/index")
     public String show() {
         return "index";
     }
 
-    @RequestMapping(value = "/convert", method = RequestMethod.POST)
+   @PostMapping("/convert")
     public ModelAndView convert(@RequestParam double rate, double usd) {
         ModelAndView modelAndView = new ModelAndView("convert");
         double vnd = rate*usd;
