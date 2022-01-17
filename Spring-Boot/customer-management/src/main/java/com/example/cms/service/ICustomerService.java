77630b50321b792ecb3072fccad842e131d348c5
Hữu Trần Ngọc <ngochuutran2702@gmail.com>
2022-01-17 10:31:32 +0700

    Ứng dụng quản lý khách hàng sử dụng Spring Boot

---
+++ Spring-Boot/customer-management/src/main/java/com/example/cms/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.cms.service;
+
+import com.example.cms.model.Customer;
+import com.example.cms.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
